ae5bf75c9bd085634646fdf80a05209305e71ed7
Sam Villegas <svillegaswarre00@mylangara.ca>
2021-07-25 16:21:51 -0700

    main body responsive

--- js/utils.js
+++ js/utils.js
@@ -31,46 +31,66 @@ function setStroke(percentage, index) {
   return stroke;
 }
 window.addEventListener('load', () => {
-  logos.forEach((logo, i) => {
+  if (
+    (window.location.pathname === '/PortfolioSamVillegas/' &&
+      window.location.search.length <= 0) ||
+    window.location.pathname === '/'
+  ) {
+    logos.forEach((logo, i) => {
+      setTimeout(() => {
+        logo.style.transition =
+          'stroke-dashoffset 1s cubic-bezier(.34,-1.61,.55,1.83) 0s, fill 2s ease-in-out 2s, stroke-dasharray 3s ease-out 1s, opacity 1s ease-out';
+        logo.style.fill = '#A7F2EB';
+        // logo.style.strokeDasharray = '0';
+        logo.style.strokeDashoffset = `${setStroke(100, i)}px`;
+      }, 300 * i);
+    });
+    const svgWrapper = document.querySelector('.logo-svg-wrapper');
+    const siteTagline = document.querySelector('.site-tagline');
+    const spanLine = document.querySelector('#over-line');
+    const customLogoWrapper = document.querySelector('.logo-wrapper-custom h1');
+    const delay = 3500;
     setTimeout(() => {
-      logo.style.transition =
-        'stroke-dashoffset 2s cubic-bezier(.34,-1.61,.55,1.83) 0s, fill 2s ease-in-out 2s, stroke-dasharray 3s ease-out 1s, opacity 1s ease-out';
-      logo.style.fill = '#A7F2EB';
-      // logo.style.strokeDasharray = '0';
-      logo.style.strokeDashoffset = `${setStroke(100, i)}px`;
-    }, 300 * i);
-  });
-  const svgWrapper = document.querySelector('.logo-svg-wrapper');
-  const spanLine = document.querySelector('#over-line');
-  const customLogoWrapper = document.querySelector('.logo-wrapper-custom h1');
-  const delay = 6500;
-  setTimeout(() => {
-    svgWrapper.style.transition = 'opacity .01s ease-out';
-    svgWrapper.style.opacity = 0;
-  }, delay);
-  setTimeout(() => {
-    svgWrapper.style.opacity = 1;
-  }, delay + 200);
-  setTimeout(() => {
-    svgWrapper.style.opacity = 0.5;
-  }, delay + 400);
-  setTimeout(() => {
-    svgWrapper.style.opacity = 1;
-  }, delay + 500);
-  setTimeout(() => {
-    svgWrapper.style.opacity = 0.5;
-    spanLine.style.opacity = 1;
-  }, delay + 600);
-  setTimeout(() => {
-    svgWrapper.style.opacity = 1;
-    spanLine.style.opacity = 0.5;
-  }, delay + 700);
-  setTimeout(() => {
-    svgWrapper.style.opacity = 0;
-    spanLine.style.opacity = 1;
-  }, delay + 900);
-  setTimeout(() => {
+      svgWrapper.style.transition = 'opacity .01s ease-out';
+      svgWrapper.style.opacity = 0;
+    }, delay);
+    setTimeout(() => {
+      svgWrapper.style.opacity = 1;
+    }, delay + 200);
+    setTimeout(() => {
+      svgWrapper.style.opacity = 0.5;
+    }, delay + 400);
+    setTimeout(() => {
+      svgWrapper.style.opacity = 1;
+    }, delay + 500);
+    setTimeout(() => {
+      svgWrapper.style.opacity = 0.5;
+      spanLine.style.opacity = 1;
+      siteTagline.style.opacity = 0.5;
+      siteTagline.style.textShadow =
+        '0px 0px 2px #a7f2eb, 0px 0px 4px rgba(242, 242, 242, 0.53)';
+    }, delay + 600);
+    setTimeout(() => {
+      svgWrapper.style.opacity = 1;
+      spanLine.style.opacity = 0.5;
+      siteTagline.style.opacity = 0;
+    }, delay + 700);
+    setTimeout(() => {
+      svgWrapper.style.opacity = 0;
+      spanLine.style.opacity = 1;
+      siteTagline.style.opacity = 0.9;
+    }, delay + 900);
+    setTimeout(() => {
+      svgWrapper.style.display = 'none';
+      customLogoWrapper.style.opacity = 1;
+      siteTagline.style.opacity = 1;
+      siteTagline.style.textShadow = 'none';
+    }, delay + 950);
+  } else {
+    const svgWrapper = document.querySelector('.logo-svg-wrapper');
+    const customLogoWrapper = document.querySelector('.logo-wrapper-custom h1');
     svgWrapper.style.display = 'none';
     customLogoWrapper.style.opacity = 1;
-  }, delay + 950);
+    siteTagline.style.opacity = 1;
+  }
 });
--- template-parts/content-bio.php
+++ template-parts/content-bio.php
@@ -3,18 +3,21 @@
 
 <div class="author-name">
   <h2 >
-
-    <?php the_field('authors_name');?>
+    <?php the_title();?>
   </h2>
+  <h3>
+    <?php the_field('authors_name');?>
+  </h3>
 </div>
 <div class="entry-thumbnail">
         <img src="<?php the_field('author_pic');?>" alt="">
 </div>
 <div class="author-tagline">
-  <h3><?php the_field('author_tagline');?></h3>
+  <h4>
+    <?php the_field('author_tagline');?>
+  </h4>
 </div>
 <div class="author-bio">
-  <h2>Who am I?</h2>
     <p >
       <?php the_field('bio');?>
     </p>
