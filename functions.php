0bca88540205c628878c5fc4a12b1c3920702d65
Sam Villegas <svillegaswarre00@mylangara.ca>
2021-07-25 17:01:09 -0700

    header fonts changed to Source Code Pro

--- functions.php
+++ functions.php
@@ -22,7 +22,7 @@ add_action('after_setup_theme', 'a7SamuelVillegas_setup');
  
 function a7SamuelVillegas_scripts_styles(){
    wp_enqueue_style('a7SamuelVillegas_style', get_stylesheet_uri());
-   wp_enqueue_style('my_first_theme_googlefonts', 'https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,300;0,400;0,700;1,300;1,400;1,700',array(),null);
+   wp_enqueue_style('my_first_theme_googlefonts', 'https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,300;0,400;0,700;1,300;1,400;1,700&family=Source+Code+Pro:ital,wght@0,300;0,400;0,700;1,300;1,400;1,700',array(),null);
    wp_enqueue_style('my_first_theme_font_awesome', 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css');
    wp_enqueue_script('customLoopTheme_script_utils_js', get_template_directory_uri() . '/js/utils.js', array(), null, true);
    wp_enqueue_script('customLoopTheme_script_navigation_js', get_template_directory_uri() . '/js/navigation.js', array(), null, true);
