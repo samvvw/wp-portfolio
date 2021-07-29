4b6ea437effedef8b18941c0644721b5983953b1
Sam Villegas <svillegaswarre00@mylangara.ca>
2021-07-29 15:05:38 -0700

    live site icon updated 1

--- template-parts/content-projects.php
+++ template-parts/content-projects.php
@@ -12,8 +12,8 @@
     <?php the_field('project_description');?>
   </p>
   <div class="project-links">
-    <a href="<?php the_field('repo_link');?>"><span class="visually-hidden">GitHub Repo</span></a>
-    <a href="<?php the_field('link_to_live_site');?>"><span class="visually-hidden">Live Site</span></a>
+    <a href="<?php the_field('repo_link');?>" target="blank"><span class="visually-hidden">GitHub Repo</span></a>
+    <a href="<?php the_field('link_to_live_site');?>" target="blank"><span class="visually-hidden">Live Site</span></a>
   </div>
 </div>
 <div class="entry-thumbnail">
